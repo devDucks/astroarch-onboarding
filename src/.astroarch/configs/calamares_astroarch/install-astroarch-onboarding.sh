ae139c429cd7f28e554bf86f00ae0a5adbec6d04
Stéphane Carlin <scarlin74@gmail.com>
2024-09-26 20:51:20 +0200

    reduction of files and configurations-2

---
+++ src/.astroarch/configs/calamares_astroarch/install-astroarch-onboarding.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cp -R "/usr/share/calamares/.astroarch/configs/calamares_astroarch" "/home/astronaut/.astroarch/configs/"
+cp -R "/usr/share/calamares/.astroarch/plugins/power_max_current" "/home/astronaut/.astroarch/plugins/"
+rm -f /root/.oh-my-zsh
+ln -s /home/astronaut/.oh-my-zsh /root
+rm -f /root/.astroarch
+ln -s /home/astronaut/.astroarch /root
+rm -f /root/.zshrc
+ln -s /home/astronaut/.astroarch/configs/.zshrc /root/.zshrc
+chsh -s /usr/bin/zsh root
