8f1e410225d5102842fb31f7fb5bf128d12d1809
sc74 <scarlin74@gmail.com>
2024-09-27 19:25:10 +0000

    Update for startup and remove files

--- src/.astroarch/configs/calamares_astroarch/run-astroarch-onboarding.sh
+++ src/.astroarch/configs/calamares_astroarch/run-astroarch-onboarding.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+# Add folders to .astroarch
 cp -R "/usr/share/calamares/.astroarch/configs/calamares_astroarch" "/home/astronaut/.astroarch/configs/"
 cp -R "/usr/share/calamares/.astroarch/plugins/power_max_current" "/home/astronaut/.astroarch/plugins/"
+
+# Link folders to root folder
 rm -f /root/.oh-my-zsh
 ln -s /home/astronaut/.oh-my-zsh /root
 rm -f /root/.astroarch
@@ -8,4 +12,6 @@ ln -s /home/astronaut/.astroarch /root
 rm -f /root/.zshrc
 ln -s /home/astronaut/.astroarch/configs/.zshrc /root/.zshrc
 chsh -s /usr/bin/zsh root
-calamares
+
+# link to folder autostart
+ln -s /usr/share/calamares/.astroarch/configs/calamares_astroarch/AstroArch-onboarding.desktop /home/astronaut/.config/autostart/AstroArch-onboarding.desktop
